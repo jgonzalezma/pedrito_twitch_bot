cb6d1ac89e79481d1866eab2a059e30ca2847767
jgonzalezma <josu0805@gmail.com>
2024-02-20 12:02:53 +0100

    version 1.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,5 +7,13 @@ Todos los cambios serán anotados en este archivo.
 
 ## [1.1.0] - 15-02-2024
 ### Added
-- Se ha agregado un tiempo de espera cada vez que escribe el bot (entre 1 y 10 segundos en principio) para que parezca más natural
-- Se ha limitado los mensajes para que máximo escriba 100 caracteres sin que corte palabras ya que escribía demasiado texto muchas veces
+- Se ha agregado un tiempo de espera cada vez que escribe el bot (entre 1 y 10 segundos en principio) para que parezca más natural.
+- Se ha limitado los mensajes para que máximo escriba 100 caracteres sin que corte palabras ya que escribía demasiado texto muchas veces.
+
+## [1.2.0] - 17-02-2024
+### Added
+- Se ha agregado una probabilidad del 5% de poder saltarse el máximo de carácteres para que muy de vez en cuando, pueda escribir textos más largos.
+- Se ha agregado la función de que el bot pueda responder de vez en cuando.
+- Se ha agregado una probabilidad del 7% de poder sacar un mensaje de "chat.txt" sin encadenar.
+### Changed
+- Se ha cambiado el límite de carácteres de 100 a 115.
--- mi_aniv.py
+++ mi_aniv.py
@@ -1,4 +1,3 @@
-import os
 import random
 import markovify
 import asyncio
@@ -40,38 +39,61 @@ class Bot(commands.Bot):
         # Incrementar el contador de mensajes
         self.messages_since_last_print += 1
 
-        """with open("chat.txt", encoding="utf-8", mode="a") as f:
-            f.write(ctx.content + "\n")"""
+        #Guarda todo lo que se escribe en el chat en chat.txt
+        with open("chat.txt", encoding="utf-8", mode="a") as f:
+            f.write(ctx.content + "\n")
 
-        if self.messages_since_last_print >= random.randint(1, 2):
+        if self.messages_since_last_print >= random.randint(10, 16):
             self.messages_since_last_print = 0
             # Construir modelo de Markov con las palabras de longitud 1-3
+            model = markovify.Text(self.text)
+            # Generar una oración
+            if random.random() < 0.07:
+                with open("chat.txt", encoding="utf-8", mode="r") as f:
+                    lines = f.readlines()
+                    # Select a random line from the file
+                    response = random.choice(lines)
+            else:
+                response = model.make_sentence()
+            # Verificar si el modelo de Markov está generando un mensaje
+            if response:
+                # Limitar la longitud a 100 caracteres sin dividir palabras
+                if random.random() < 0.05:
+                    await asyncio.sleep(random.uniform(1, 10))
+                    await ctx.channel.send(response)
+                else:
+                    if len(response) > 115:
+                        response = response[:115].rsplit(' ', 1)[0]
+                        # Esperar un tiempo aleatorio entre 1 y 7 segundos
+                        await asyncio.sleep(random.uniform(1, 2))
+                        await ctx.channel.send(response)
+            else:
+                print("El modelo de Markov no generó un mensaje.")
+        
+            # Verificar si el mensaje es una respuesta a un mensaje existente en el chat
+        elif ctx.content.startswith('@'):
+            # Crear modelo de Markov solo con el mensaje anterior
             model = markovify.Text(self.text)
             # Generar una oración
             response = model.make_sentence()
             # Verificar si el modelo de Markov está generando un mensaje
             if response:
-                # Limitar la longitud a 100 caracteres sin dividir palabras
-                if len(response) > 100:
-                    response = response[:100].rsplit(' ', 1)[0]
+                # Limitar la longitud a 110 caracteres sin dividir palabras
+                if len(response) > 110:
+                    response = response[:110].rsplit(' ', 1)[0]
                 # Esperar un tiempo aleatorio entre 1 y 7 segundos
-                await asyncio.sleep(random.uniform(1, 2))
+                await asyncio.sleep(random.uniform(1, 7))
                 await ctx.channel.send(response)
             else:
                 print("El modelo de Markov no generó un mensaje.")
 
-        # Since we have commands and are overriding the default `event_message`
-        # We must let the bot know we want to handle and invoke our commands...
-        await self.handle_commands(ctx)
+            # Since we have commands and are overriding the default `event_message`
+            # We must let the bot know we want to handle and invoke our commands...
+            await self.handle_commands(ctx)
 
     @commands.command()
     async def hola(self, ctx: commands.Context):
-        # Here we have a command hello, we can invoke our command with our prefix and command name
-        # e.g ?hello
-        # We can also give our commands aliases (different names) to invoke with.
-
-        # Send a hello back!
-        # Sending a reply back to the channel is easy... Below is an example.
+        # comando !hola
         await ctx.send(f'Hola uwu {ctx.author.name}!')
 
 
